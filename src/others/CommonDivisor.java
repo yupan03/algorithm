f66faeb1373e03bde2d08671ffe529bbe0b956e9
yupan03 <735997736@qq.com>
2019-11-28 09:20:43 +0800

    创建项目

---
+++ src/others/CommonDivisor.java
@@ -0,0 +1,36 @@
+package others;
+
+public class CommonDivisor {
+    public static void main(String[] args) {
+        System.out.println(greatestCommonDivisor(100, 1020));
+        System.out.println(getMaxNum(12));
+    }
+
+    /**
+     * 欧几米得算法求最大公约数
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int greatestCommonDivisor(int x, int y) {
+        if (y == 0) return x;
+        return greatestCommonDivisor(y, x % y);
+    }
+
+    public static int getMaxNum(int n) {
+        int k = 0;
+
+        while (n / 3 > 0) {
+            k += n / 3;
+            n = n / 3 + n % 3;
+        }
+
+        if (n % 3 == 2) {
+            // 如果最后剩下两个, 像老板借一个，然后加1
+            k += 1;
+        }
+
+        return k;
+    }
+}
